ee2bb82dcc26b5bc9d13e0ebf808196b63f6a97e
arefin5 <arefintalukder5@gmail.com>
2024-06-24 15:32:13 +0600

    fix-project

--- project.clj
+++ project.clj
@@ -1,88 +1,37 @@
 (defproject calculator "0.1.0-SNAPSHOT"
+  :description "Your project description"
+  :url "http://example.com"
 
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-
-  :dependencies [[buddy/buddy-auth "3.0.323"]
+  ;; Define project dependencies
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [buddy/buddy-auth "3.0.323"]
                  [buddy/buddy-core "1.10.413"]
-                 [buddy/buddy-hashers "1.8.158"]
-                 [buddy/buddy-sign "3.4.333"]
-                 [ch.qos.logback/logback-classic "1.4.4"]
-                 [clojure.java-time "1.1.0"]
-                 [conman "0.9.5"]
-                 [cprop "0.1.19"]
-                 [expound "0.9.0"]
-                 [funcool/struct "1.4.0"]
-                 [json-html "0.4.7"]
-                 [luminus-migrations "0.7.5"]
-                 [luminus-transit "0.1.5"]
-                 [luminus-undertow "0.1.18"]
-                 [luminus/ring-ttl-session "0.3.3"]
-                 [markdown-clj "1.11.3"]
-                 [metosin/muuntaja "0.6.8"]
-                 [metosin/reitit "0.5.18"]
-                 [metosin/ring-http-response "0.9.3"]
-                 [mount "0.1.16"]
-                 [nrepl "1.0.0"]
-                 [org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.cli "1.0.214"]
-                 [org.clojure/tools.logging "1.2.4"]
-                 [org.webjars.npm/bulma "0.9.4"]
-                 [org.webjars.npm/material-icons "1.10.8"]
-                 [org.webjars/webjars-locator "0.45"]
-                 [org.webjars/webjars-locator-jboss-vfs "0.1.0"]
-                 [org.xerial/sqlite-jdbc "3.36.0.3"]
-                 [ring-webjars "0.2.0"]
+                 ;; Add all your dependencies here
                  [ring/ring-core "1.9.6"]
                  [ring/ring-defaults "0.3.4"]
-                 [selmer "1.12.55"]]
+                 [ring/ring-devel "1.9.6"]
+                 [org.xerial/sqlite-jdbc "3.36.0.3"]
+                 ;; Add any other dependencies your project requires
+                 ]
 
-  :min-lein-version "2.0.0"
-  
-  :source-paths ["src/clj"]
-  :test-paths ["test/clj"]
-  :resource-paths ["resources"]
-  :target-path "target/%s/"
-  :main ^:skip-aot calculator.core
+  ;; Define project profiles (e.g., for dev, test, uberjar)
+  :profiles {:dev {:dependencies [[com.jakemccrary/lein-test-refresh "0.24.1"]
+                                  [pjstadig/humane-test-output "0.11.0"]
+                                  [ring/ring-devel "1.9.6"]
+                                  [ring/ring-mock "0.4.0"]]
+                   :resource-paths ["env/dev/resources"]
+                   :jvm-opts ["-Dconf=dev-config.edn"]
+                   :source-paths ["env/dev/clj"]}
 
-  :plugins [] 
+             :test {:dependencies [[pjstadig/humane-test-output "0.11.0"]]
+                    :resource-paths ["env/test/resources"]
+                    :jvm-opts ["-Dconf=test-config.edn"]}
 
-  :profiles
-  {:uberjar {:omit-source true
-             :aot :all
-             :uberjar-name "calculator.jar"
-             :source-paths ["env/prod/clj" ]
-             :resource-paths ["env/prod/resources"]}
+             :uberjar {:aot :all
+                       :omit-source true
+                       :source-paths ["env/prod/clj"]
+                       :resource-paths ["env/prod/resources"]
+                       :main calculator.core
+                       :uberjar-name "calculator.jar"}}
 
-   :dev           [:project/dev :profiles/dev]
-   :test          [:project/dev :project/test :profiles/test]
-
-   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn" ]
-                  :dependencies [[org.clojure/tools.namespace "1.3.0"]
-                                 [pjstadig/humane-test-output "0.11.0"]
-                                 [prone "2021-04-23"]
-                                 [ring/ring-devel "1.9.6"]
-                                 [ring/ring-mock "0.4.0"]]
-                  :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]
-                                 [jonase/eastwood "1.2.4"]
-                                 [cider/cider-nrepl "0.26.0"]] 
-                  
-                  :source-paths ["env/dev/clj" ]
-                  :resource-paths ["env/dev/resources"]
-                  :repl-options {:init-ns user
-                                 :timeout 120000}
-                  :injections [(require 'pjstadig.humane-test-output)
-                               (pjstadig.humane-test-output/activate!)]}
-   :project/test {:jvm-opts ["-Dconf=test-config.edn" ]
-                  :resource-paths ["env/test/resources"] }
-   :profiles/dev {}
-   :profiles/test {}})
-:dependencies [[org.clojure/clojure "1.11.1"]
-               ;; other dependencies
-               [ring/ring-mock "0.4.0"]
-               [pjstadig/humane-test-output "0.11.0"]]
-
-:profiles {:dev {:dependencies [[ring/ring-devel "1.9.6"]]
-                 :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]}}
-
-:main ^:skip-aot calculator.core
+  :main calculator.core)
