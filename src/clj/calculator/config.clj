83be65e80e5aa1a96d48ea515818f7e98341b7ad
arefin5 <arefintalukder5@gmail.com>
2024-06-24 15:43:14 +0600

    fix-config

--- src/clj/calculator/config.clj
+++ src/clj/calculator/config.clj
@@ -3,11 +3,3 @@
     [cprop.core :refer [load-config]]
     [cprop.source :as source]
     [mount.core :refer [args defstate]]))
-
-(defstate env
-  :start
-  (load-config
-    :merge
-    [(args)
-     (source/from-system-props)
-     (source/from-env)]))
